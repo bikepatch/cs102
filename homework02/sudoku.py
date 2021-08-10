bd412232faa28b25ba9e73b0c23147ed063e3b7e
Ivan <ivanivanfomenko@gmail.com>
2021-08-10 17:35:02 +0300

    solve

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -149,7 +149,21 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    position = find_empty_positions(grid)
+    if position is None:
+        return grid
+    else:
+        possible_values = find_possible_values(grid, position)
+        if possible_values is None:
+            return grid
+        for val in possible_values:
+            grid[position[0]][position[1]] = val
+            solve(grid)
+            if find_empty_positions(grid) is not None:
+                continue
+            else:
+                return grid
+        grid[position[0]][position[1]] = '.'
 
 
 def check_solution(solution: tp.List[tp.List[str]]) -> bool:
