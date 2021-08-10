ae3f00263b0a0f65251dea349eed8d8330144caa
Ivan <ivanivanfomenko@gmail.com>
2021-08-10 17:28:52 +0300

    find_empty_positions

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -110,7 +110,12 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    pass
+    length = len(grid)
+    for i in range(length):
+        for j in range(length):
+            if grid[i][j] == '.':
+                return i, j
+    return None
 
 
 def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.Set[str]:
