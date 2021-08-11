1edf7e3a21d03ce44f129f8e35d326e11f10eff8
Ivan <ivanivanfomenko@gmail.com>
2021-08-11 13:07:49 +0300

    passed unittests

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -10,13 +10,27 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border(0)
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for i in range(self.life.rows):
+            for j in range(self.life.cols):
+                if not self.life.curr_generation[i][j]:
+                    screen.addch(i + 1, j + 1, ' ')
+                else:
+                    screen.addch(i + 1, j + 1, '*')
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        screen.clear()
+        self.draw_borders(screen)
+        screen.refresh()
+        flag = True
+        while flag:
+            self.draw_grid(screen)
+            screen.refresh()
+            self.life.step()
+            if self.life.is_max_generations_exceed:
+                flag = False
         curses.endwin()
