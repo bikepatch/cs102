23c558a0eaee57fb288df0e3a51879ef4bfd0e06
Ivan <ivanivanfomenko@gmail.com>
2021-08-12 13:44:42 +0300

    refs

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -3,30 +3,55 @@ import typing as tp
 
 
 def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    path = gitdir / ref
+    with open(path, "w") as file:
+        file.write(new_value)
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
     # PUT YOUR CODE HERE
-    ...
+    pass
 
 
 def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    path = gitdir / refname
+    if refname == "HEAD":
+        with open(path, "r") as f:
+            info = f.read()
+        refname = info[info.find(" ") + 1 :].strip()
+    path = gitdir / refname
+    if path.exists() is not False:
+        with open(path, "r") as f:
+            info = f.read()
+        return info
+    else:
+        return None
 
 
 def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
-    # PUT YOUR CODE HERE
-    ...
+    resolve = ref_resolve(gitdir, "HEAD")
+    if resolve is not None:
+        return resolve
+    else:
+        return None
 
 
 def is_detached(gitdir: pathlib.Path) -> bool:
-    # PUT YOUR CODE HERE
-    ...
+    path = gitdir / "HEAD"
+    with open(path, "r") as f:
+        info = f.read()
+    if not info.startswith("ref:"):
+        return True
+    else:
+        return False
 
 
 def get_ref(gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    path = gitdir / "HEAD"
+    with open(path, "r") as f:
+        info = f.read()
+    if is_detached(gitdir):
+        return info
+    else:
+        ref = info[info.find(" ") + 1 :].strip()
+        return ref
