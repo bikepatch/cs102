adf5659cb8970d0cd0e4ebc45bfc703128372fd1
Ivan Fomenko <ivanivanfomenko@gmail.com>
2021-08-24 20:17:42 +0300

    more isort

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -6,7 +6,6 @@ from string import Template
 
 import pandas as pd
 from pandas import json_normalize
-
 from vkapi import config, session
 from vkapi.config import VK_CONFIG
 from vkapi.exceptions import APIError
